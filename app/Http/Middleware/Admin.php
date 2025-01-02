b116a33b56405e57df0a6cc9a944d2b4f76d002d
Ziad Talaat <ziadsnake33@gmail.com>
2025-01-02 21:32:38 +0200

    Added auth check before checking role to avoid exceptions

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -3,21 +3,22 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Response;
 
 class Admin
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     * @param Request $request
+     * @param Closure(Request): (Response|RedirectResponse) $next
+     * @return Response|RedirectResponse
      */
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next): Response|RedirectResponse
     {
-        if (Auth::user()->is_admin)
+        if (auth()->check() && auth()->user()->is_admin)
             return $next($request);
         else
             return redirect('/dashboard');
